f7767ae21e424dc5bf44ffbef6b7d9abcc652e5f
leandororochaadm <leandrorochaadm@gmail.com>
2020-03-27 23:26:57 -0400

    configurando todas as mensagem sobre o peso

--- lib/main.dart
+++ lib/main.dart
@@ -30,9 +30,17 @@ class _HomeState extends State<Home> {
     double imc = weight / (height * height);
     setState(() {
       if (imc < 18.6) {
-        _info = "Abaixo do peso (${imc.toStringAsPrecision(4)})";
-      } else {
-        _info = "";
+        _info = "Abaixo do peso (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _info = "Peso ideal (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _info = "Levemente acima do peso (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _info = "Obesidade grau I (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 34.9 && imc < 39.9) {
+        _info = " Obesidade grau II (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 39.9) {
+        _info = "Obesidade grau III (${imc.toStringAsPrecision(3)})";
       }
     });
   }
